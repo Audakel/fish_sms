8e3c3050986cfe08ce91510db869783542c70682
audakel <audakel@gmail.com>
2015-11-29 11:06:21 -0700

    pic update

--- client/app/team/team.controller.js
+++ client/app/team/team.controller.js
@@ -5,13 +5,18 @@ function TeamController($scope, $http, socket) {
   var self = this; 
   $scope.teamMembers = [{
       name: 'Austin',
-      about: 'Used Redis + Memcached + Node.js + Go + MongoDB + MariaDB + postgres + Clojure to help scale his fishing start-up (from 10 to 20 hits per day) ಠ_ರೃ'
+      about: 'Used Redis + Memcached + Node.js + Go + MongoDB + MariaDB + postgres + Clojure to help scale his fishing start-up (from 10 to 20 hits per day) ಠ_ರೃ',
+      pic: "assets/images/audakel.png"
     }, {
       name: 'Cole',
-      about: 'Creater of fshng.js. His post "Why Go Is Better Than You And Everything You Own And Why It Always Will Be" front page of NYT at #2 spot'
+      about: 'Creater of fshng.js. His post "Why Go Is Better Than You And Everything You Own And Why It Always Will Be" front page of NYT at #2 spot',
+      pic: "assets/images/cole.png"
+
     }, {
       name: 'Matt',
-      about: 'More details on matt'
+      about: 'More details on matt',
+      pic: "assets/images/matt.png"
+
     }];
 
   // $scope.teamMembers = [
