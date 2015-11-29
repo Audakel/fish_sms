6ae56b91bc555b8905f562c8ce36c08bddbdf09c
audakel <audakel@gmail.com>
2015-11-29 12:28:46 -0700

    pic update

--- client/app/team/team.controller.js
+++ client/app/team/team.controller.js
@@ -9,7 +9,7 @@ function TeamController($scope, $http, socket) {
       pic: "assets/images/audakel.png"
     }, {
       name: 'Cole',
-      about: 'Creater of fshng.js. His post "Why Go Is Better Than You And Everything You Own And Why It Always Will Be" front page of NYT at #2 spot',
+      about: 'Creater of fshng.js. His post "Why Go Is Better Than You And Everything You Own And Why It Always Will Be" was featured on the front page of NYT at #2 spot',
       pic: "assets/images/cole.png"
 
     }, {
